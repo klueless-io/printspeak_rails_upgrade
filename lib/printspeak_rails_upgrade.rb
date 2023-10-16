f9cdfb10a7260967fa1b9efb47178e1835424796
David Cruwys <david@ideasmen.com.au>
2023-10-17 07:21:51 +1000

    first commit

---
+++ lib/printspeak_rails_upgrade.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "printspeak_rails_upgrade/version"
+
+module PrintspeakRailsUpgrade
+  class Error < StandardError; end
+  # Your code goes here...
+end
